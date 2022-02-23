6065f9ee83b694e9f33e030515c2f50206a22ab5
Nitro <railian.dina@gmail.com>
2022-02-23 12:02:26 +0200

    Masiv

--- Temă pentruacasa/java.js
+++ Temă pentruacasa/java.js
@@ -176,7 +176,9 @@ console.log(styles);*/
   
   alert( sumInput() );*/
   //exercitiul 5
-/*let numar = [1, 2, 3, 4, 5, 6, 7, 8, 9];  
-numar[4] = '0, 0, 0'
-console.log(numar);*/
+let numar = [1, 2, 3, 4, 5, 6, 7, 8, 9];  
+numar.splice(4,1, "0", "0","0");
+console.log(numar);
+//exercitiul8
+//const color =[“red”, “green”, “blue”];
 

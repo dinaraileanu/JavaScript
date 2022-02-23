0b6992f4ea9469362718b505c5d4c6b1ae0e9c17
Nitro <railian.dina@gmail.com>
2022-02-23 11:03:01 +0200

    Masive

--- Temă pentruacasa/java.js
+++ Temă pentruacasa/java.js
@@ -74,7 +74,7 @@ else {
   }
   
   alert( ucFirst("dina") );*/
-  //exercitiul 5
+//exercitiul 5
 /*
 function extractCurrencyValue(str){
   str.splice(1);
@@ -124,13 +124,59 @@ else {
     else{
         alert("Poti incepe chiar acum");
     }*/
-    // exercitiul 10
-    var numar = prompt("Introduceti numarul ")
-    
-for (var i=1; i<=numar; i++) {
-  if (numar%i==0) {
-    console.log(i);
-  }
-}
-
+// exercitiul 10
+/*var numar = prompt("Introduceti numarul ")
  
+for (var i=1; i<=numar; i++) {
+if (numar%i==0) {
+console.log(i);
+}
+}*/
+//Arrays
+//Exercitiul 3
+/*let styles = ["Jazz", "Blues"];
+console.log(styles);
+styles.push("Rock and Roll");
+console.log(styles);
+styles[1] = "Clasica"
+console.log(styles);
+alert(styles.shift());
+console.log(styles);
+styles.unshift("Rap", "Reggy");
+console.log(styles);*/
+//Exercitiul 1
+//let numar = ["1",'2',"3","4","5"];
+/*for (let i = 0; i < numar.length; i++) {
+    console.log(numar);
+}*/
+/*for (let numere of numar){
+    console.log(numar);
+}*/
+//exercitiul 4
+/*function sumInput() {
+
+    let numbers = [];
+  
+    while (true) {
+  
+      let value = prompt("Introduceti", 0);
+  
+      
+      if (value === "" || value === null || !isFinite(value)) break;
+  
+      numbers.push(+value);
+    }
+  
+    let sum = 0;
+    for (let number of numbers) {
+      sum += number;
+    }
+    return sum;
+  }
+  
+  alert( sumInput() );*/
+  //exercitiul 5
+/*let numar = [1, 2, 3, 4, 5, 6, 7, 8, 9];  
+numar[4] = '0, 0, 0'
+console.log(numar);*/
+
